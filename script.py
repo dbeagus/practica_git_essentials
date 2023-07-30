3058153976e70dc2793174fd294d5b4e6445a417
dbeagus <dbeagus@gmail.com>
2023-07-30 19:04:23 -0300

    Agrego procedimientos para aproximar funciones

--- script.py
+++ script.py
@@ -1,5 +1,32 @@
-import numpy as np
-e = np.e
+import sympy as sp #Usado para simbolos, derivadas y manejo de funciones
+e = sp.Symbol("e")
+x = sp.Symbol("x")
+y = sp.Symbol("y")
 
-def function(x, y):
+# Definir la función que se desea aproximar
+def funcion(x, y):
     return e**(-x**2-y**2)
+
+#Aproximacion por serie de Taylor
+class AproximacionTaylor:
+    def __init__(self, funcion):
+        self.funcion = funcion
+        self.x = x
+        self.y = y
+
+    #Metodo para ver la funcion que se esta evaluando
+    def ver(self):
+        return str(self.funcion)
+    
+    #Metodo para aproximar la funcion en un punto
+    def aproximar(self, grado, x0, y0):
+        aproximacion = funcion(x0,y0)
+        
+        #derivadas mediante el uso de sp.diff
+        df_dx = sp.diff(self.funcion, self.x).subs(self.x, x)
+        df_dy = sp.diff(self.funcion, self.y).subs(self.y, y)
+        
+        for i in range(1, grado+1):
+            aproximacion +=  (df_dx * (self.x - x0) + df_dy * (self.y - y0)) / sp.factorial(i)
+        
+        return aproximacion
